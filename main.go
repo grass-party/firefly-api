0f1e9a6a2048d22e86d47be034d05ba4a0e7ee5e
qodot <waitingforqodot@gmail.com>
2018-05-18 23:01:43 +0900

    gorm을 이용한 mysql 접속 테스트

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
+	userName := "root"
+	userPassword := "rootpassword"
+	dbHost := "rdb"
+	dbPort := "3306"
+	dbName := "firefly"
+	dbUrl := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?allowNativePasswords=true", userName, userPassword, dbHost, dbPort, dbName)
+
+	db, err := gorm.Open("mysql", dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
